55e23371f760b68c170bba8fb067b1b47a75040e
João Edson <joaoedson.dantas@outlook.com>
2023-10-29 22:15:51 -0300

    feat: finalizando a aplicação

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-namespace Stopwatch
+﻿namespace Stopwatch
 {
     class Program
     {
@@ -13,12 +10,36 @@ namespace Stopwatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundo");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
 
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
 
+            int multiplirs = 1;
+
+            if (type == 'm')
+                multiplirs = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplirs);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("GO ...");
+            Thread.Sleep(2500);
+            Start(time);
         }
 
         static void Start(int time)
@@ -36,6 +57,7 @@ namespace Stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch Finalizado");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
